af5cceb2a3c248bc6d72f154268ef48ce94c52c2
mosimann.nicolas@gmail.com <mosimann.nicolas@gmail.com@04ff5802-12e5-c1a2-1b8b-92983a29892a>
2012-05-10 09:10:51 +0000

    change

--- trunk/ eventplanner --username marco.serpi@gmail.com/EventPlanner/src/db/UserDAO.java
+++ trunk/ eventplanner --username marco.serpi@gmail.com/EventPlanner/src/db/UserDAO.java
@@ -1,19 +1,43 @@
 package db;
 
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+
 import model.User;
 import bl.*;
 
 
 
 public class UserDAO {
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private User currentUser;
 	
-	public String answer;
+	private String answer;
 	
 	
 	public User login(String userName, String passWord) {
 		this.answer = DB_Connector.request("SELECT * FROM tbl_user WHERE username = " + userName+ " AND password = " + passWord);
+		if (this.answer == "") {
 
-		return null;
+			// error message
+			JOptionPane.showMessageDialog(null,"Login failed! Wrong Username or Password", "Error",JOptionPane.OK_CANCEL_OPTION);
+			return null;
+
+		} else {
+			StringTokenizer st = new StringTokenizer(answer, ",");
+			userName = st.nextToken();
+			passWord = st.nextToken();
+			firstName = st.nextToken();
+			lastName = st.nextToken();
+			dateOfBirth = st.nextToken();
+			this.currentUser = new User(userName, passWord, firstName, lastName, dateOfBirth);
+			return currentUser;
+			
+
+		}
 
 	}
 	public User regCheck(String userName, String passWord, String firstName, String lastName, String dateOfBirth){
@@ -25,4 +49,3 @@ public class UserDAO {
 		return null;
 	}
 }
-
