491751e09024c3d0658a58108ce6268ab6062db3
mosimann.nicolas@gmail.com <mosimann.nicolas@gmail.com@04ff5802-12e5-c1a2-1b8b-92983a29892a>
2012-05-10 07:59:41 +0000

    updated

--- trunk/ eventplanner --username marco.serpi@gmail.com/EventPlanner/src/db/UserDAO.java
+++ trunk/ eventplanner --username marco.serpi@gmail.com/EventPlanner/src/db/UserDAO.java
@@ -1,13 +1,28 @@
 package db;
 
 import model.User;
+import bl.*;
+
+
 
 public class UserDAO {
-	public User login(){
-		
-		
+	
+	public String answer;
+	
+	
+	public User login(String userName, String passWord) {
+		this.answer = DB_Connector.request("SELECT * FROM tbl_user WHERE username = " + userName+ " AND password = " + passWord);
+
+		return null;
+
+	}
+	public User regCheck(String userName, String passWord, String firstName, String lastName, String dateOfBirth){
+		this.answer = DB_Connector.request("SELECT * FROM tbl_user WHERE username = " + userName+ " AND password = " + passWord+ " AND firstName = " + firstName+ "AND lastName = " + lastName+ "AND dateOfBirth = " + dateOfBirth);
+		return null;
+	}
+	public User registration(String userName, String passWord, String firstName, String lastName, String dateOfBirth){
 		
 		return null;
-		
 	}
 }
+
