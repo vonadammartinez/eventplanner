b4a6624f740b6e0bac6daa87ba6f47233548e4a8
mosimann.nicolas@gmail.com <mosimann.nicolas@gmail.com@04ff5802-12e5-c1a2-1b8b-92983a29892a>
2012-05-10 08:00:11 +0000

    updated

---  eventplanner --username marco.serpi@gmail.com/EventPlanner/src/bl/UserSession.java
+++  eventplanner --username marco.serpi@gmail.com/EventPlanner/src/bl/UserSession.java
@@ -1,6 +1,5 @@
 package bl;
 
-
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -12,23 +11,23 @@ import model.User;
 import gui.*;
 
 public class UserSession {
-	
+
 	private User currentUser;
 	private String answer;
-	
+
 	private String userName;
 	private String passWord;
 	private String lastName;
 	private String firstName;
 	private String dateOfBirth;
-	
-	public UserSession(String userName, String passWord){
-		this.answer = DB_Connector.request("SELECT * FROM tbl_user WHERE username = " + userName + " AND password = " + passWord);
-		if (this.answer == ""){
-			
+
+	public UserSession(String userName, String passWord) {
+		
+		if (this.answer == "") {
+
 			// error message
-			JOptionPane.showMessageDialog(null, "Login failed! Wrong Username or Password", "Error", JOptionPane.OK_CANCEL_OPTION);
-			
+			JOptionPane.showMessageDialog(null,"Login failed! Wrong Username or Password", "Error",JOptionPane.OK_CANCEL_OPTION);
+
 		} else {
 			StringTokenizer st = new StringTokenizer(answer, ",");
 			userName = st.nextToken();
@@ -39,8 +38,20 @@ public class UserSession {
 			this.currentUser = new User(userName, passWord, firstName, lastName, dateOfBirth);
 			// Open EventList
 			GuiController.EventListstarter();
+
+		}
+	}
+	public static void Registration(String userName, String passWord, String firstName, String lastName, String dateOfBirth){
+		if (){
+			JOptionPane.showMessageDialog(null,"Registration failed! User or UserName already exists", "Error",JOptionPane.OK_CANCEL_OPTION);
+
+			
+		}else{
+			
 			
 		}
+		
+		
 	}
 
 }
